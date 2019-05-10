e001d30b4bd412fc38ee2911e0cddac8c452b5b3
victorN <victornguyen082@gmail.com>
2019-05-10 13:19:58 -0400

    delete function

--- village/src/App.js
+++ village/src/App.js
@@ -35,6 +35,16 @@ class App extends Component {
     })
   }
 
+  deleteSmurf = id => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => {
+        this.setState({ smurfs: res.data });
+        // this.props.history.push('/');
+      })
+      .catch(err => console.log(err));
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -46,7 +56,8 @@ class App extends Component {
            <SmurfForm {...props} addSmurf={this.addSmurf}/>}
            />    
         <Route exact path="/" render={props => 
-        <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Smurfs {...props} smurfs={this.state.smurfs}
+        deleteSmurf={this.deleteSmurf} />} />
 
       </div>
     );
